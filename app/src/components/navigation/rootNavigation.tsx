5248e11e486640ffc601105782cff20d16d0445f
merveguzl <merveguzel.iletisim@gmail.com>
2025-03-25 17:15:19 +0300

    -l

--- app/src/components/navigation/rootNavigation.tsx
+++ app/src/components/navigation/rootNavigation.tsx
@@ -1,22 +1,22 @@
 "use client";
 import "../../../../i18n";
-import useUserStore from "@/app/src/store/user";
+//import useUserStore from "@/app/src/store/user";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/app/src/lib/queryClient";
 import MainNavigation from "./mainNavigation";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+//import { useRouter } from "next/navigation";
+//import { useEffect } from "react";
 
 export default function RootNavigation() {
-  const { isLogin } = useUserStore();
+  // const { isLogin } = useUserStore();
 
-  const router = useRouter();
+  //  const router = useRouter();
 
-  useEffect(() => {
-    if (!isLogin) {
-      router.push("/login");
-    }
-  });
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     router.push("/login");
+  //   }
+  // });
 
   return (
     <>
