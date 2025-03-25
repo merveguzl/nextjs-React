c3f3ebcd6f42b6249f99085f8c7f303117f6abaa
merveguzl <merveguzel.iletisim@gmail.com>
2025-03-25 16:51:45 +0300

    -fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import RootNavigation from "@/app/src/components/navigation/rootNavigation";
+import { Suspense } from "react";
 export default function App() {
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen bg-white">
-      <RootNavigation />
-    </div>
+    <Suspense fallback={<>Loading...</>}>
+      <div className="grid grid-rows-[auto_1fr_auto] min-h-screen bg-white">
+        <RootNavigation />
+      </div>
+    </Suspense>
   );
 }
